3ae72867acbffa85829075bd4e9b4914ae0c8319
samy <samytwo22@gmail.com>
2024-03-23 23:53:35 +0100

    Inscription

--- app/src/main/java/com/example/web_app/ConfirmationSignalment.java
+++ app/src/main/java/com/example/web_app/ConfirmationSignalment.java
@@ -2,13 +2,26 @@ package com.example.web_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ConfirmationSignalment extends AppCompatActivity {
+
+    private Button Btn_retour_pprincipale;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_confirmation_signalment);
+        Btn_retour_pprincipale = findViewById(R.id.btn_retour_pprincipale);
+
+        Btn_retour_pprincipale.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(ConfirmationSignalment.this, MainActivity.class));
+            }
+        });
     }
 }
