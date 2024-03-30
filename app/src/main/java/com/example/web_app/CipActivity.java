792a465adf953e10f1ada81513f487d9e52e7002
Fsv6 <112781646+Fsv6@users.noreply.github.com>
2024-03-30 23:53:16 +0100

    nouvelle base cip et début parametre

--- app/src/main/java/com/example/web_app/CipActivity.java
+++ app/src/main/java/com/example/web_app/CipActivity.java
@@ -13,17 +13,22 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 public class CipActivity extends AppCompatActivity {
 
     private EditText codeCipEditText;
+    private EditText descriptionEditText;
     private Button validateButton;
     private FirebaseFirestore firestore;
 
@@ -34,7 +39,8 @@ public class CipActivity extends AppCompatActivity {
 
         firestore = FirebaseFirestore.getInstance();
 
-        codeCipEditText = findViewById(R.id.usernameEditText);
+        codeCipEditText = findViewById(R.id.cipEditText);
+        descriptionEditText = findViewById(R.id.descriptionEditText);
         validateButton = findViewById(R.id.validateButton);
 
         validateButton.setOnClickListener(new View.OnClickListener() {
@@ -47,11 +53,14 @@ public class CipActivity extends AppCompatActivity {
 
     private void validateCip() {
         String codeCipStr = codeCipEditText.getText().toString().trim();
+        String description = descriptionEditText.getText().toString().trim();
         long codeCip = Long.parseLong(codeCipStr);
         if (TextUtils.isEmpty(codeCipStr)){
             Toast.makeText(this, "Veuillez entrer un code CIP", Toast.LENGTH_SHORT).show();
             return;
         }
+        String userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
         firestore.collection("Medicaments")
                 .whereEqualTo("CIP13", codeCip)
                 .get()
@@ -61,13 +70,8 @@ public class CipActivity extends AppCompatActivity {
                         if (task.isSuccessful()) {
                             QuerySnapshot document = task.getResult();
                             if (!document.isEmpty()){
-                                Map<String, Object> updates = new HashMap<>();
-                                updates.put("signale_manquant", "true");
-                                firestore.collection("Medicaments")
-                                        .document(document.getDocuments().get(0).getId())
-                                        .update(updates);
-                                Intent intent = new Intent(CipActivity.this, ConfirmationSignalment.class);
-                                startActivity(intent);
+                                String denomination = document.getDocuments().get(0).getString("Denomination");
+                                addSignalement(codeCip, denomination, userId, description);
                             } else {
                                 AlertDialog.Builder builder = new AlertDialog.Builder(CipActivity.this);
                                 builder.setTitle("Code CIP invalide");
@@ -78,6 +82,31 @@ public class CipActivity extends AppCompatActivity {
                         } else {
                             Toast.makeText(CipActivity.this, "Erreur : " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                         }
+                    }
+                });
+
+    }
+    private void addSignalement(long codeCip, String designation, String userId, String description){
+        Map<String, Object> signalement = new HashMap<>();
+        signalement.put("CIP13", codeCip);
+        signalement.put("designation", designation);
+        signalement.put("user_id", userId);
+        signalement.put("Date", new Date());
+        signalement.put("traité", false);
+        signalement.put("description", description);
+
+        firestore.collection("Signalements").add(signalement)
+                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Intent intent = new Intent(CipActivity.this, ConfirmationSignalment.class);
+                        startActivity(intent);
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(CipActivity.this, "Erreur : " + e.getMessage(), Toast.LENGTH_SHORT).show();
                     }
                 });
     }
--- app/src/main/java/com/example/web_app/MainActivity.java
+++ app/src/main/java/com/example/web_app/MainActivity.java
@@ -6,14 +6,12 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.DatePickerDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import kotlin.Unit;
 
@@ -28,11 +26,13 @@ import com.journeyapps.barcodescanner.CaptureActivity;
 import com.journeyapps.barcodescanner.ScanContract;
 import com.journeyapps.barcodescanner.ScanOptions;
 
+import java.lang.reflect.Parameter;
+
 import kotlin.jvm.functions.Function1;
 
 public class MainActivity extends AppCompatActivity {
     protected final int home = 1;
-    protected final int liste = 2;
+    protected final int param = 2;
     protected final int profil = 3;
     private TextView TextViewGreeting;
     private FirebaseFirestore fStor;
@@ -68,7 +68,7 @@ public class MainActivity extends AppCompatActivity {
         MeowBottomNavigation bottomNavigation = findViewById(R.id.bottomnav);
         bottomNavigation.add(new MeowBottomNavigation.Model(profil, R.drawable.baseline_person_24));
         bottomNavigation.add(new MeowBottomNavigation.Model(home, R.drawable.baseline_home_24));
-        bottomNavigation.add(new MeowBottomNavigation.Model(liste, R.drawable.baseline_list_24));
+        bottomNavigation.add(new MeowBottomNavigation.Model(param, R.drawable.ic_parametres));
         bottomNavigation.show(1, true);
         bottomNavigation.setOnClickMenuListener(new Function1<MeowBottomNavigation.Model, Unit>() {
             @Override
@@ -85,17 +85,14 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent;
                 switch (model.getId()) {
                     case home:
-                        // Créer un Intent pour l'activité Home
                         intent = new Intent(MainActivity.this, MainActivity.class);
                         startActivity(intent);
                         break;
-                    case liste:
-                        // Créer un Intent pour l'activité Dashboard
-                        intent = new Intent(MainActivity.this, ConfirmationSignalment.class);
+                    case param:
+                        intent = new Intent(MainActivity.this, ParameterActivity.class);
                         startActivity(intent);
                         break;
                     case profil:
-                        // Créer un Intent pour l'activité Profil
                         intent = new Intent(MainActivity.this, Profile.class);
                         startActivity(intent);
                         break;
