ac00f8dede7a5fc0dc8a1ae045b38a92ce748809
samy <samytwo22@gmail.com>
2024-04-01 00:06:06 +0200

    profile

--- app/src/main/java/com/example/web_app/fragmentadmin/OneAdmin.java
+++ app/src/main/java/com/example/web_app/fragmentadmin/OneAdmin.java
@@ -2,13 +2,31 @@ package com.example.web_app.fragmentadmin;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 
 import com.example.web_app.R;
+import com.example.web_app.Signalement;
+import com.example.web_app.fragment.OneFragment;
+import com.example.web_app.signalementadapter;
+import com.example.web_app.signalementadapteradmin;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -16,51 +34,68 @@ import com.example.web_app.R;
  * create an instance of this fragment.
  */
 public class OneAdmin extends Fragment {
+    private FirebaseFirestore db;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
     public OneAdmin() {
-        // Required empty public constructor
+
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment OneFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static OneAdmin newInstance(String param1, String param2) {
-        OneAdmin fragment = new OneAdmin();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+    public static com.example.web_app.fragment.OneFragment newInstance() {
+        return new com.example.web_app.fragment.OneFragment();
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        db = FirebaseFirestore.getInstance();
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_signa_profil, container, false);
+        View view = inflater.inflate(R.layout.fragment_1admin, container, false);
+
+        ListView listView = view.findViewById(R.id.scrollView1A);
+        db.collection("Signalements")
+                .whereEqualTo("traité", false)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            List<Signalement> signalements = new ArrayList<>();
+                            for (DocumentSnapshot document : task.getResult()) {
+                                // Récupérer les données de chaque signalement et les ajouter à la liste
+                                Signalement signalement = document.toObject(Signalement.class);
+                                signalements.add(signalement);
+                            }
+
+                            // Trier les signalements par ordre croissant de leur heure
+                            Collections.sort(signalements, new Comparator<Signalement>() {
+                                @Override
+                                public int compare(Signalement s1, Signalement s2) {
+                                    return s2.getDate().compareTo(s1.getDate()); // Comparaison des timestamps
+                                }
+                            });
+
+                            // Créer et configurer l'adaptateur pour afficher les signalements dans la ListView
+                            signalementadapteradmin adapter = new signalementadapteradmin(getActivity(),
+                                    R.layout.item_signalement_admin,
+                                    signalements);
+                            listView.setAdapter(adapter);
+                        } else {
+                            // Gérer les erreurs de récupération des données depuis Firestore
+                            // Vous pouvez afficher un message d'erreur ou effectuer une autre action ici
+                            Exception e = task.getException();
+                            Log.e("Firebase", "Error getting documents: " + e);
+                        }
+                    }
+                });
+
+        return view;
     }
+
 }
